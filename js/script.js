a29f3d071619f3b32261ae50fd3138355f0ac6c9
joPay <josefina.payer@gmail.com>
2022-09-16 16:15:19 -0300

    password generated ok

--- js/script.js
+++ js/script.js
@@ -21,7 +21,8 @@ const symbols = ['$', '#', '?', '&', '+', '!'];
 const $passDisplay = $("#display-password")
 const $passItself = $("#password-itself")
 const $refreshPass = $("#refresh-token")
-const $copyPass = $("copy-password")
+const $copyPass = $("#copy-password")
+const $generatePass = $("#generate-password")
 
 
 /* *************** password option inputs ***************** */
@@ -44,15 +45,56 @@ const getRandomCharacter = (array) => {
     return randomSelection  
 } 
 
-const shuffleCharacters = (array) => {
-    for (let i = array.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+const generatePassword = () => {
+    let passArray = [];
+    while (passArray.length < 64) {
+        let character = getRandomCharacter(lowerLetters)
+        passArray.push(character)
+        character = getRandomCharacter(numbers)
+        passArray.push(character)
+        character = getRandomCharacter(symbols)
+        passArray.push(character)
+        character = getRandomCharacter(capsLetters)
+        passArray.push(character)
     }
-    return array
+    return passArray
+ }
+
+// const saveCheckedInputs = () => {
+//     let lowerChecked = $lowercase.checked
+//     let capitalChecked = $capital.checked
+//     let numberChecked = $containsNumbers.checked
+//     let symbolChecked = $containsSymbols.checked
+//     checkedArr.push(lowerChecked, capitalChecked, numberChecked, symbolChecked)
+//     return checkedArr
+// }
+
+const defineCheckedCharacters = () => {
+    let passArray = generatePassword()
+    if (!$lowercase.checked) {
+        passArray = passArray.filter((letter) => {
+            return !lowerLetters.includes(letter)
+        })
+    }
+    if (!$capital.checked) {
+        passArray = passArray.filter((letter) => {
+            return !capsLetters.includes(letter)
+        })
+    } 
+    if (!$containsNumbers.checked) {
+        passArray = passArray.filter((number) => {
+            return !numbers.includes(number)
+        })
+    }
+    if (!$containsSymbols.checked) {
+        passArray = passArray.filter((symbol) => {
+            return !symbols.includes(symbol)
+        })
+    }
+    return passArray
 }
 
-const stablishLength = () => {
+ const stablishLength = () => {
     let passwordLength;
     if ($char8.checked) {
         passwordLength = 8
@@ -66,78 +108,77 @@ const stablishLength = () => {
     return passwordLength
 } 
 
-// MAKE FUNCTION FOR PASSWORD WITH ONLY LETTERS (caps and lower)
+const cutPassword = () => {
+    let passLength = stablishLength()
+    let passToCut = defineCheckedCharacters()
+    passToCut = passToCut.slice(0,passLength)
+    return passToCut
+}         
 
-
-// MAKE FUNCTION FOR LOOPING THROUGH ARRAYS (NUMBERS AND SYMBOLS)
-
-const generatePassword = () => {
-    let password
-    let passArray = [];
-    let passwordLength = stablishLength();
-    if ($containsLetters.checked && !$containsNumbers.checked && !$containsSymbols.checked && !$capital.checked) {
-        // for (let i = 0; i < passwordLength; i++) 
-        while (passArray.length < passwordLength) {
-            let character = getRandomCharacter(lowerLetters)
-            passArray.push(character)
-        }
+const shuffleCharacters = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
     }
-    if ($containsLetters.checked && $containsNumbers.checked && !$containsSymbols.checked && !$capital.checked) {
-        // let newArray = lowerLetters.concat(numbers)
-        // for (let i = 0; i < passwordLength / 2; i++) 
-        while (passArray.length < passwordLength) {
-            let character = getRandomCharacter(lowerLetters)
-            passArray.push(character)
-            character = getRandomCharacter(numbers)
-            passArray.push(character)
-        }
-    }                         
-    if ($containsLetters.checked && $containsNumbers.checked && $containsSymbols.checked && !$capital.checked) {
-        // let newArray = lowerLetters.concat(numbers)
-        // newArray = newArray.concat(symbols)
-        // for (let i = 0; i < passwordLength / 3; i++) 
-        while (passArray.length < passwordLength) {
-            let character = getRandomCharacter(lowerLetters)
-            passArray.push(character)
-            character = getRandomCharacter(numbers)
-            passArray.push(character)
-            character = getRandomCharacter(symbols)
-            passArray.push(character)
-        }
-        // if (passArray.length === 18) {
-        //     passArray.slice(0,16)
-        // }
-        
-    }
+    return array
+}
 
-    console.log(passArray.length)
-    if (passArray.length === 9) {
-            passArray = passArray.slice(0,8)
-        }  // no anda por resolver
-        console.log(passArray.length)
-    
-    shuffleCharacters(passArray)
-    return passArray.join('')
+const getFinalPassword = () => {
+    let passwordGenerated = shuffleCharacters(cutPassword().join(''))
+    return passwordGenerated
 }
 
 const showPassOnDisplay = () => {
-    let passwordGenerated = generatePassword()
-    // for (let i = 0; i < passwordGenerated.length; i++) {
-    //     if (typeof i === 'number') {
-    //         passwordGenerated.charAt(i).style.color = "red"  // PENDIENTE RESOLVER
-    //     }
-    // }
-    $passItself.innerHTML = `${passwordGenerated}`
+    $passItself.innerHTML = `${getFinalPassword()}`
 }
 
 showPassOnDisplay()
 
-// events
+/* EVENTS */
 
-// click generate
+$copyPass.addEventListener("click", (e) => {
+    navigator.clipboard.writeText($passItself.innerText)
+})
 
-// click refresh
+$containsLetters.addEventListener("click", (e) => {
+    if (!$containsLetters.checked) {
+        $capital.checked = false
+        $lowercase.checked = false
+        $lowercase.setAttribute("disabled", '')
+        $capital.setAttribute("disabled", '')
+    }
+    if ($containsLetters.checked) {
+        $lowercase.removeAttribute("disabled", '')
+        $capital.removeAttribute("disabled", '')
+        $lowercase.checked = true
+        $capital.checked = true
+    }
+})
 
-// click copy to clipboard
+$lowercase.addEventListener("click", (e) => {
+    if (!$lowercase.checked && !$capital.checked) {
+        $containsLetters.setAttribute("disabled", '')
+    }
+    if ($lowercase.checked || $capital.checked) {
+        $containsLetters.removeAttribute("disabled", '')
+    }
+})
 
-// make an event change that disables lower and caps checkbox if user doesn't check letter options
+$capital.addEventListener("click", (e) => {
+    if (!$lowercase.checked && !$capital.checked) {
+        $containsLetters.setAttribute("disabled", '')
+    }
+    if ($lowercase.checked || $capital.checked) {
+        $containsLetters.removeAttribute("disabled", '')
+    }
+})
+
+$generatePass.addEventListener("click", (e) => {
+    showPassOnDisplay()
+})
+
+// cómo vergas hago esto??? 
+$refreshPass.addEventListener("click", (e) => {
+    showPassOnDisplay()
+})
+
